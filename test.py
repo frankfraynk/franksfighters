fa2dde49e4298d6241363b786e48e24d3a134daa
frankfraynk <35715299+frankfraynk@users.noreply.github.com>
2018-09-20 13:33:20 -0500

    wip battle system
    progress!

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import random
 class Player: #Create a class for the player with different attributes
     def __init__(self, name):
         self.name = name
@@ -8,8 +9,6 @@ class Player: #Create a class for the player with different attributes
         self.gold = 0
         self.pots = 0
         self.speed = 4
-    def attack():
-        global enemy
         
 PlayerIG = Player('Jim')
 
@@ -19,24 +18,30 @@ class Enemy:
         self.dmg = dmg
         self.dtype = dtype
         self.name = name
-        self.speed = random.randint(2,5)
-    def enemySpawn():
-        print(name + " has spawned! It has a size of " + size +'.')
-    def attackPlayer():
+        self.speed = random.randint(2,3)
+        self.size = 2
+        self.health = 4
+
+    def attackPlayer(self):
         print("Enemy is swinging!")
-        hitchance = random.randint(1,20) + (lvl/2)
+        hitchance = random.randint(1,20) + (self.lvl/2)
         if hitchance >= (12+PlayerIG.armor):
             PlayerIG.health = PlayerIG.health - self.dmg
-class Fight:
-    def __init__(player,enemy):
-        Player.__init__(self,name)
-        Enemy.__init__(self,name)
-        self.player = p
-        self.enemy = e
-    def detFirst():
-        if player.speed
+def Fight(enemy):
+    print(enemy.name + " has spawned! It has a size of " + str(enemy.size) +', and a speed of ' + str(enemy.speed))
+    if enemy.speed <= PlayerIG.speed:
+        turn = 0
+    else:
+        turn = 1
 
+    while PlayerIG.health > 0 or enemy.health > 0:
+        if turn == 0:
+            uchoice = input("attack, or run?")
+            if uchoice == 'attack' or uchoice == 'a':
+                enemy.health = enemy.health - PlayerIG.attack
+                print(PlayerIG.name + " has hit the " + enemy.name + "for " + str(PlayerIG.attack) + " damage!")
 zombie = Enemy(3,7,'Slam','Zombie')
 print("Welcome to Frank's battle simulator!")
 print("Our contenders today are " + zombie.name + ", and " + PlayerIG.name +".")
-print(Let the battle commence
+print('Let the battle commence!')
+Fight(zombie)
